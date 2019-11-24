18a03d4e9dcd6e1874fd728b908a1d19a233c9df
Nadongjun <nadongjun@Nadongjunui-MacBookPro.local>
2019-11-24 20:52:55 +0900

    commit

---
+++ app/src/main/java/com/howl/movingrestaurant/navigation/model/userDTO.kt
@@ -0,0 +1,12 @@
+package com.howl.movingrestaurant.navigation.model
+
+data class userDTO(
+
+    var uid: String,
+    var email: String,
+    var name: String,
+    var gender: String,
+    var age: String,
+    var local: String,
+    var userId : String
+    )
